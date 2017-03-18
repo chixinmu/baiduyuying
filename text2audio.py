31bb123d88e9cebd3b991d5a2c743a504ba554fb
chixinmu <445465396@qq.com>
2017-03-18 22:46:10 +0800

    del personal data

--- text2audio.py
+++ text2audio.py
@@ -8,16 +8,16 @@ TOKEN_URL = 'https://openapi.baidu.com/oauth/2.0/token?grant_type={0}&client_id=
 TEXT2AUDIO_URL = 'http://tsn.baidu.com/text2audio?tex={0}&lan=zh&cuid={1}&ctp=1&tok={2}&spd={3}&pit={4}&vol={5}&per={6}'
 GRANT_TYPE = 'client_credentials'
 CUID = 'pyandi_orangepiplus2e'
-TTS_AK = 'DYGhXNnRUwD2TQyL3b8XYDM8'
-TTS_SK = 'qM36yNKGqODW0eufxHBIQx7w23pRVrpu'
+TTS_AK = ''
+TTS_SK = ''
 
 
 
 def get_token_from_baidu():
     url = 'https://openapi.baidu.com/oauth/2.0/token'
     payload = { 'grant_type':'client_credentials',
-                'client_id':'DYGhXNnRUwD2TQyL3b8XYDM8',
-                'client_secret':'qM36yNKGqODW0eufxHBIQx7w23pRVrpu'
+                'client_id':'',
+                'client_secret':''
                 }
     r = requests.post(url,params=payload)
     if r.status_code == 200:
